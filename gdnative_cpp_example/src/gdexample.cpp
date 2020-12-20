bcea5868c5bc724b89b7930982e08c36d4953a5d
2shady4u <jd16581556@gmail.com>
2020-12-20 14:39:01 +0100

    Add example GDNative code

---
+++ gdnative_cpp_example/src/gdexample.cpp
@@ -0,0 +1,27 @@
+#include "gdexample.h"
+
+using namespace godot;
+
+void GDExample::_register_methods() {
+    register_method("_process", &GDExample::_process);
+}
+
+GDExample::GDExample() {
+}
+
+GDExample::~GDExample() {
+    // add your cleanup here
+}
+
+void GDExample::_init() {
+    // initialize any variables here
+    time_passed = 0.0;
+}
+
+void GDExample::_process(float delta) {
+    time_passed += delta;
+
+    Vector2 new_position = Vector2(10.0 + (10.0 * sin(time_passed * 2.0)), 10.0 + (10.0 * cos(time_passed * 1.5)));
+
+    set_position(new_position);
+}
