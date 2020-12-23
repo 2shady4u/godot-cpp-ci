19f05d67baf7095072b7ad65702d97395a6f1dde
2shady4u <jd16581556@gmail.com>
2020-12-23 14:43:41 +0100

    Remove extension from LOCAL_MODULE

--- gdnative_cpp_example/Android.mk
+++ gdnative_cpp_example/Android.mk
@@ -7,7 +7,7 @@ LOCAL_SRC_FILES := godot-cpp/bin/libgodot-cpp.android.debug.arm64v8.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := demo/bin/android/arm64-v8a/libgdexample.so
+LOCAL_MODULE := demo/bin/android/arm64-v8a/libgdexample
 LOCAL_CPPFLAGS := -std=c++14
 LOCAL_CPP_FEATURES := rtti exceptions
 LOCAL_LDLIBS := -llog
